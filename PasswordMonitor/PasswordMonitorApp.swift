6a140e64aa986cbb4d3e4b75566b1fa4e960b20d
Kamil Popowicz <kamilpopowicz@icloud.com>
2026-01-25 13:04:38 +0100

    Initial Commit

---
+++ PasswordMonitor/PasswordMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordMonitorApp.swift
+//  PasswordMonitor
+//
+//  Created by Kamil Popowicz on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PasswordMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
